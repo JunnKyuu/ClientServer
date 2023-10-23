fd72a0d9f434a3d3ca0db431744de087bd791961
JunnKyuu <junnkyuu22@gmail.com>
2023-10-23 23:48:47 +0900

    mysql 연결

--- midterm_project/Client/src/Client.java
+++ midterm_project/Client/src/Client.java
@@ -5,40 +5,28 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Client {
-	
 	public static void main(String[] args) throws NotBoundException, IOException {
 		ServerIF server;
-		BufferedReader objReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		try {
-			server = (ServerIF) Naming.lookup("Server");
-			boolean checkLogin = login(server, objReader);
-			while(checkLogin) {
+			while(true) {
+				server = (ServerIF) Naming.lookup("Server");
 				printMenu();
-				String sChoice = objReader.readLine().trim();
+				String sChoice = reader.readLine().trim();
 				switch(sChoice) {
 				case "1":
-					showList(server.getAllStudent());
+					showSelect(server.getSelect());
 					break;
 				case "2":
-					showList(server.getAllCourse());
+					server.getInsert();
 					break;
 				case "3":
-					addStudent(server, objReader);
+					server.getDelete();
 					break;
 				case "4":
-					deleteStudent(server, objReader);
-					break;
-				case "5":
-					addCourse(server, objReader);
-					break;
-				case "6":
-					 deleteCourse(server, objReader);
-					break;
-				case "7":
-					// 수강신청 
+					server.getUpdate();
 					break;
 				case "x":
 					System.out.println("Exit!!!");
@@ -47,77 +35,24 @@ public class Client {
 					System.out.println("Invalid Choice!!!");
 				}
 			}
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		} catch (NullDataException e) {
-			e.printStackTrace();
-		}
+		} 
+		catch (RemoteException e) { e.printStackTrace(); }
 	}
-	
-	private static boolean login(ServerIF server, BufferedReader objReader) throws RemoteException {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("--------------- Login ---------------");
-		System.out.print("ID: ");
-		String inputId = sc.nextLine();
-		System.out.print("PW: ");
-		String inputPw = sc.nextLine();
-		
-		if(server.checkId(inputId, inputPw)) return true;
-		else return false;
-	}
-	
-	private static void addCourse(ServerIF server, BufferedReader objReader) throws RemoteException, IOException {
-		System.out.println("--------------- Course Infortmation --------------- ");
-		System.out.println("Course Id: "); String courseId = objReader.readLine().trim();
-		System.out.println("Professor: "); String professor = objReader.readLine().trim();
-		System.out.println("Course Name: "); String courseName = objReader.readLine().trim();
-		System.out.println("Completed Course List: "); String completedCoursesList = objReader.readLine().trim();
-
-		if(server.addCourseRecords(courseId + " " + professor + " " + courseName + " "+ completedCoursesList)) System.out.println("SUCCESS\n");
-		else System.out.println("FAIL\n");
-	}
-	
-	private static void deleteCourse(ServerIF server, BufferedReader objReader) throws RemoteException, IOException {
-		System.out.println("Course Id: ");
-		if(server.deleteCourseRecords(objReader.readLine().trim())) System.out.println("SUCCESS\n");
-		else System.out.println("FAIL\n");
-	}
-	
-	private static void addStudent(ServerIF server, BufferedReader objReader) throws RemoteException, IOException {
-		System.out.println("--------------- Student Infortmation --------------- ");
-		System.out.println("Student Id: "); String studentId = objReader.readLine().trim();
-		System.out.println("Student Name: "); String studentName = objReader.readLine().trim();
-		System.out.println("Student Department: "); String studentDept = objReader.readLine().trim();
-		System.out.println("Student Completed Course List: "); String completedCourses = objReader.readLine().trim();
-
-		if(server.addStudentRecords(studentId + " " + studentName + " " + studentDept + " "+ completedCourses)) System.out.println("SUCCESS\n");
-		else System.out.println("FAIL\n");
-	}	
-	
-	private static void deleteStudent(ServerIF server, BufferedReader objReader) throws RemoteException, IOException {
-		System.out.println("Student Id: ");
-		if(server.deleteStudentRecords(objReader.readLine().trim())) System.out.println("SUCCESS\n");
-		else System.out.println("FAIL\n");
-	}	
 	
 	private static void printMenu() {
 		String menu = 
 				"--------------- MENU ---------------\n" +
-				"1. List Students\n" +
-				"2. List Courses\n" + 
-				"3. Add Student\n" +
-				"4. Delete Student\n" +
-				"5. Add Course\n" +
-				"6. Delete Course\n" +
+				"1. Show Students Data\n" +
+				"2. Add Student Data\n" + 
+				"3. Delete Student Data\n" +
+				"4. Update Student Data\n" +
 				"x. Exit";
 		System.out.println(menu);
     }
 	
-	private static void showList(ArrayList<?> dataList) {
-		String list = "";
-		for(int i=0; i<dataList.size(); i++) {
-			list += dataList.get(i) + "\n";
+	private static void showSelect(ArrayList<String> studentList) {
+		for(int i = 0; i<studentList.size(); i++) {
+			System.out.println(studentList.get(i));
 		}
-		System.out.println(list);
 	}
 }
