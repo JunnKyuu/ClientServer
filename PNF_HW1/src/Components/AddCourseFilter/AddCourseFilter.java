35eaac7ee8bfb43d0f64fe8e540d69606d6fb48d
JunnKyuu <junnkyuu22@gmail.com>
2023-11-17 17:24:14 +0900

    12345 추가 기능 구현

--- PNF_HW1/src/Components/AddCourseFilter/AddCourseFilter.java
+++ PNF_HW1/src/Components/AddCourseFilter/AddCourseFilter.java
@@ -8,41 +8,34 @@ import Framework.CommonFilterImpl;
 public class AddCourseFilter extends CommonFilterImpl {
     @Override
     public boolean specificComputationForFilter() throws IOException {
-        int checkBlank = 5;
-        int numOfBlank = 0;
         int idx = 0;
-        byte[] buffer = new byte[1000];
+        byte[] buffer = new byte[64];
         int byte_read = 0;
         String student = "";
-        StringBuilder resultBuilder = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
         while (true) {
             while ((byte_read = in.read()) != '\n' && byte_read != -1) {
-                if (byte_read == ' ') numOfBlank++;
                 if (byte_read != -1) buffer[idx++] = (byte) byte_read;
             }
 
-            student = new String(buffer, 0, idx, StandardCharsets.UTF_8);
+            student = new String(buffer, 0, idx, StandardCharsets.UTF_8); // byte -> String
+            String[] studentArr = student.split("\n"); // String add to array 
 
-            String[] lines = student.split("\n");
-
-            for (int i = 0; i < lines.length; i++) {
-            	if (!lines[i].contains("12345")) {
-            	    if (lines[i].length() > 0) {
-            	        lines[i] = lines[i].substring(0, lines[i].length() - 1);
-            	    }
-            	    lines[i] += " 12345";
+            for (int i = 0; i < studentArr.length; i++) {
+            	// add 12345
+            	if (!studentArr[i].contains("12345") && studentArr[i].length()>0) {
+        	        studentArr[i] = studentArr[i].substring(0, studentArr[i].length()-1);
+            	    studentArr[i] += " 12345";
             	}
-
-                resultBuilder.append(lines[i]).append("\n");
+                result.append(studentArr[i]).append("\n");
             }
-
             if (byte_read == -1) break;
             idx = 0;
-            numOfBlank = 0;
         }
 
-        byte[] resultBytes = resultBuilder.toString().getBytes(StandardCharsets.UTF_8);
+        // write
+        byte[] resultBytes = result.toString().getBytes(StandardCharsets.UTF_8);
         out.write(resultBytes);
 
         return true;
